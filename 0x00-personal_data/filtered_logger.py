719a4431f2c4627783c65199054f4367db4edf89
Demonware023 <awamchimere@icloud.com>
2024-08-29 21:39:30 -0700

    Regex-ing

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -1,23 +1,24 @@
 #!/usr/bin/env python3
 """
-This module contains the function filter_datum which obfuscates 
+This module contains the function filter_datum which obfuscates
 certain fields in a log message.
 """
 
 import re
 from typing import List
 
-def filter_datum(fields: List[str], redaction: str, 
+
+def filter_datum(fields: List[str], redaction: str,
                  message: str, separator: str) -> str:
     """
     Obfuscates certain fields in a log message.
-    
+
     Args:
         fields (List[str]): Fields to obfuscate.
         redaction (str): Replacement string for obfuscation.
         message (str): Log line to obfuscate.
         separator (str): Field separator in the log line.
-    
+
     Returns:
         str: The obfuscated log message.
     """
