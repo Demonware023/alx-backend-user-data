316e5ff4186faf39027e0f209e91128a6fa3781c
Demonware023 <awamchimere@icloud.com>
2024-08-29 21:42:30 -0700

    Log formatter

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -1,29 +1,47 @@
 #!/usr/bin/env python3
 """
-This module contains the function filter_datum which obfuscates
-certain fields in a log message.
+This module provides a filtered logger for obfuscating sensitive data.
 """
 
 import re
+import logging
 from typing import List
 
 
-def filter_datum(fields: List[str], redaction: str,
-                 message: str, separator: str) -> str:
+def filter_datum(fields: List[str], redaction: str, message: str,
+                 separator: str) -> str:
     """
-    Obfuscates certain fields in a log message.
-
-    Args:
-        fields (List[str]): Fields to obfuscate.
-        redaction (str): Replacement string for obfuscation.
-        message (str): Log line to obfuscate.
-        separator (str): Field separator in the log line.
-
-    Returns:
-        str: The obfuscated log message.
+    Obfuscates specified fields in a log message.
     """
     for field in fields:
-        pattern = f'{field}=[^{separator}]*'
-        replacement = f'{field}={redaction}'
-        message = re.sub(pattern, replacement, message)
+        pattern = f'{field}=.*?{separator}'
+        replace = f'{field}={redaction}{separator}'
+        message = re.sub(pattern, replace, message)
     return message
+
+
+class RedactingFormatter(logging.Formatter):
+    """
+    Redacting Formatter class.
+    """
+
+    REDACTION = "***"
+    FORMAT = ("[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: "
+              "%(message)s")
+    SEPARATOR = ";"
+
+    def __init__(self, fields: List[str]):
+        """
+        Initialize the formatter with specific fields to redact.
+        """
+        super().__init__(self.FORMAT)
+        self.fields = fields
+
+    def format(self, record: logging.LogRecord) -> str:
+        """
+        Format the log record by redacting sensitive information.
+        """
+        original_message = super().format(record)
+        filtered_message = filter_datum(self.fields, self.REDACTION,
+                                        original_message, self.SEPARATOR)
+        return filtered_message
