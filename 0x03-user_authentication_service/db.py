45a8fb176a232f221455da4c8d83afab5a0e75f8
Demonware023 <Awamchimere@icloud.com>
2024-09-11 07:20:56 -0700

    Find user

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -6,7 +6,8 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.orm.session import Session
-
+from sqlalchemy.orm.exc import NoResultFound
+from sqlalchemy.exc import InvalidRequestError
 from user import Base, User
 
 
@@ -32,16 +33,34 @@ class DB:
         return self.__session
 
     def add_user(self, email: str, hashed_password: str) -> User:
-        """Adds a new user to the database and returns the User object
-
-        Args:
-            email (str): user's email
-            hashed_password (str): user's hashed password
-
-        Returns:
-            User: the user object that was added to the database
-        """
+        """Adds a new user to the database and returns the User object"""
         new_user = User(email=email, hashed_password=hashed_password)
         self._session.add(new_user)
         self._session.commit()
         return new_user
+
+    def find_user_by(self, **kwargs) -> User:
+        """Finds a user by arbitrary keyword arguments.
+
+        Args:
+            kwargs: Arbitrary keyword arguments to filter the users table.
+
+        Returns:
+            User: The user that matches the provided filters.
+
+        Raises:
+            NoResultFound: If no user matches the filter.
+            InvalidRequestError: If an invalid column is provided.
+        """
+        try:
+            # Try to find the user using filter_by
+            user = self._session.query(User).filter_by(**kwargs).first()
+
+            if user is None:
+                raise NoResultFound()
+
+            return user
+
+        except InvalidRequestError:
+            # This will catch errors like invalid column names
+            raise InvalidRequestError()
